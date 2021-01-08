c132dd20b1a834a8ac2b49c6e98ce9aa16e08c22
fatemeh.as <fatemehaminsafaei@gmail.com>
2021-01-08 20:25:35 +0330

    python tutorial

---
+++ python/key_feature.py
@@ -0,0 +1,21 @@
+#What is key features of Python?
+#Python is a dynamic, high level, free open source and interpreted programming language.
+# It supports object-oriented programming as well as procedural oriented programming.
+# In Python, we don't need to declare the type of variable because it is a dynamically typed language
+
+
+"""Features in Python
+There are many features in Python, some of which are discussed below –
+
+1. Easy to code:
+Python is a high-level programming language. Python is very easy to learn the language as compared to other languages like C, C#, Javascript, Java, etc. It is very easy to code in python language and anybody can learn python basics in a few hours or days. It is also a developer-friendly language.
+
+2. Free and Open Source:
+Python language is freely available at the official website and you can download it from the given download link below click on the Download Python keyword.
+Download Python
+Since it is open-source, this means that source code is also available to the public. So you can download it as, use it as well as share it.
+
+3. Object-Oriented Language:
+One of the key features of python is Object-Oriented programming. Python supports object-oriented language and concepts of classes, objects encapsulation, etc.
+
+"""
