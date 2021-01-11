81e07d73d57993b284e83ff2c781a045268e9ad2
fatemeh.as <fatemehaminsafaei@gmail.com>
2021-01-11 20:01:16 +0330

    python tutorial

--- python/Calculator.py
+++ python/Calculator.py
@@ -1,3 +1,6 @@
+import sys
+
+
 class Calculator:
     def __init__(self):
         self.number1 = 0
@@ -5,4 +8,62 @@ class Calculator:
         self.result = 0
 
     def sum(self):
-        return self.number1 + self.number2
+        self.result = self.number1 + self.number2
+
+    def substraction(self):
+        self.result = self.number1 - self.number2
+
+    def multiplication(self):
+        self.result = self.number1 * self.number2
+
+    def divition(self):
+        self.result = self.number1 / self.number2
+
+    def power(self):
+        self.result = self.number1 ** self.number2
+
+
+object = Calculator()
+
+while True:
+    print("\n Calculator")
+    print("\n 1.ADD \n 2.SUBSTRACTION \n 3.MULTIPLY \n 4.DIVISION \n 5.POWER \n 6.EXIT")
+
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1:
+        object.number1 = int(input("Enter number 1: "))
+        object.number2 = int(input("Enter number 2: "))
+        object.sum()
+        print("RESULT = ", object.result)
+
+    elif choice == 2:
+        object.number1 = int(input("Enter number 1: "))
+        object.number2 = int(input("Enter number 2: "))
+        object.substraction()
+        print("RESULT = ", object.result)
+
+    elif choice == 3:
+        object.number1 = int(input("Enter number 1: "))
+        object.number2 = int(input("Enter number 2: "))
+        object.multiplication()
+        print("RESULT = ", object.result)
+
+    elif choice == 4:
+        object.number1 = int(input("Enter number 1: "))
+        object.number2 = int(input("Enter number 2: "))
+        object.divition()
+        print("RESULT = ", object.result)
+
+    elif choice == 5:
+        object.number1 = int(input("Enter number 1: "))
+        object.number2 = int(input("Enter number 2: "))
+        object.power()
+        print("RESULT = ", object.result)
+
+    elif choice == 6:
+        print("EXIT")
+        sys.exit()
+
+    else:
+        print("Invalid Choice")
---
+++ test/test.py
@@ -0,0 +1,71 @@
+import sys
+
+
+# -------------------------------------------Business Logic Layer
+class calculator:
+    def __init__(self):
+        self.no1 = 0
+        self.no2 = 0
+        self.res = 0
+
+    def sum(self):
+        self.res = self.no1 + self.no2
+
+    def substraction(self):
+        self.res = self.no1 - self.no2
+
+    def multiplication(self):
+        self.res = self.no1 * self.no2
+
+    def division(self):
+        self.res = self.no1 / self.no2
+
+    def power(self):
+        self.res = self.no1 ** self.no2
+
+
+# ---------------------------------------------Presentation Layer
+
+obj = calculator()
+while True:
+    print("\n Calculator")
+    print("\n 1. ADD \n 2. SUBSTRACTION \n 3. MULTIPLY \n 4. DIVISION \n 5. POWER \n 6. EXIT")
+
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1:
+        obj.no1 = int(input("Enter number 1: "))
+        obj.no2 = int(input("Enter number 2: "))
+        obj.sum()
+        print("Result = ", obj.res)
+
+    elif choice == 2:
+        obj.no1 = int(input("Enter number 1: "))
+        obj.no2 = int(input("Enter number 2: "))
+        obj.substraction()
+        print("Result = ", obj.res)
+
+    elif choice == 3:
+        obj.no1 = int(input("Enter number 1: "))
+        obj.no2 = int(input("Enter number 2: "))
+        obj.multiplication()
+        print("Result = ", obj.res)
+
+    elif choice == 4:
+        obj.no1 = int(input("Enter number 1: "))
+        obj.no2 = int(input("Enter number 2: "))
+        obj.division()
+        print("Result = ", obj.res)
+
+    elif choice == 5:
+        obj.no1 = int(input("Enter number 1: "))
+        obj.no2 = int(input("Enter number 2: "))
+        obj.power()
+        print("Result = ", obj.res)
+
+    elif choice == 6:
+        print("Exit successfully")
+        sys.exit()
+
+    else:
+        print("Invalid Choice")
