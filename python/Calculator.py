1c81f52f225c9ff599d932c852b4c4f561d01c31
fatemeh.as <fatemehaminsafaei@gmail.com>
2021-01-11 19:44:11 +0330

    python tutorial

---
+++ python/Calculator.py
@@ -0,0 +1,8 @@
+class Calculator:
+    def __init__(self):
+        self.number1 = 0
+        self.number2 = 0
+        self.result = 0
+
+    def sum(self):
+        return self.number1 + self.number2
